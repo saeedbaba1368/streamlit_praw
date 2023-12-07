7de28e07199b2acbd279b919a17da1beca5dccd1
saeedbaba1368 <108682089+saeedbaba1368@users.noreply.github.com>
2023-12-08 00:05:32 +0330

    Update 0_👋_hello.py

--- pages/0_👋_hello.py
+++ pages/0_👋_hello.py
@@ -7,27 +7,17 @@ import pandas as pd
 import csv
 import time
 from praw.models import MoreComments
+
 reddit = praw.Reddit(client_id=client_id,
 	             client_secret=client_secret,	
 	             user_agent=user_agent,
                      check_for_async=False,
                      ratelimit_seconds=1)
-col1, col2 = st.columns(2)
-with col1:
-    name = st.text_input('Enter your topic for reddit scraper')
-with col2:
-    limit= st.number_input('Enter your limit number  for posts', min_value=0, max_value=100, value=4, step=1)
-button = st.button("click to start process")
-if button:
-	
-	st.success(f'Welcome to the reddit scraper for , {name} topic!')
-	subreddit = reddit.subreddit(name)
+def get_reddit_data(topic, limit):
 	posts_dict = {"Title": [], "Post Text": [],
           "ID": [], "Score": [],
           "Total Comments": [], "Post URL": []}
-	
-        
-        
+	subreddit = reddit.subreddit(topic)
 	for submission in subreddit.hot(limit=int(limit)):
 		posts_dict["Title"].append(submission.title)
 		posts_dict["Post Text"].append(submission.selftext)
@@ -35,6 +25,22 @@ if button:
 		posts_dict["Score"].append(submission.score)
 		posts_dict["Total Comments"].append(submission.num_comments)
 		posts_dict["Post URL"].append(submission.url)
+	return pd.DataFrame(posts_dict)
+col1, col2 = st.columns(2)
+with col1:
+    topic = st.text_input('Enter your topic for reddit scraper')
+with col2:
+    limit= st.number_input('Enter your limit number  for posts', min_value=0, max_value=100, value=4, step=1)
+button = st.button("click to start process")
+if button:
+	
+	st.success(f'Welcome to the reddit scraper for , {name} topic!')
+	df = get_reddit_data(topic,limit)
+
+	
+        
+        
+
 	df = pd.DataFrame(posts_dict)
 	st.dataframe(posts_dict)
 	

66a6b6fbdaa4da1b4b3bc5bed5275b4840dbaae3
saeedbaba1368 <108682089+saeedbaba1368@users.noreply.github.com>
2023-12-07 13:12:43 +0330

    Add files via upload

---
+++ pages/0_👋_hello.py
@@ -0,0 +1,36 @@
+client_secret="sCyU2xJ0AsGNfMKqZKorMfUyGJrUJg"
+user_agent="Acceptable_Bill7915 "
+client_id="I4EphV1-XBhKrTL8fIxf2Q"
+import streamlit as st
+import praw
+import pandas as pd
+from praw.models import MoreComments
+reddit = praw.Reddit(client_id=client_id,		 # your client id
+							client_secret=client_secret,	 # your client secret
+							user_agent=user_agent,
+              check_for_async=False,
+              ratelimit_seconds=1)	 # your user agen
+subreddit = reddit.subreddit("BookRecommendations")
+posts_dict = {"Title": [], "Post Text": [],
+			"ID": [], "Score": [],
+			"Total Comments": [], "Post URL": []
+			}
+for submission in subreddit.hot(limit=3):
+	# Title of each post
+	posts_dict["Title"].append(submission.title)
+
+	# Text inside a post
+	posts_dict["Post Text"].append(submission.selftext)
+
+	# Unique ID of each post
+	posts_dict["ID"].append(submission.id)
+
+	# The score of a post
+	posts_dict["Score"].append(submission.score)
+
+	# Total number of comments inside the post
+	posts_dict["Total Comments"].append(submission.num_comments)
+
+	# URL of each post
+	posts_dict["Post URL"].append(submission.url)
+st.write(posts_dict)

616b84f4561f1f8511b46940ebb53db367edfa9e
saeedbaba1368 <108682089+saeedbaba1368@users.noreply.github.com>
2023-12-07 22:50:13 +0330

    Update 0_👋_hello.py

--- pages/0_👋_hello.py
+++ pages/0_👋_hello.py
@@ -12,59 +12,53 @@ reddit = praw.Reddit(client_id=client_id,		 # your client id
               check_for_async=False,
               ratelimit_seconds=1)	 # your user agen
 name = st.text_input('Enter your topic for reddit scraper')
-button2 = st.button("click to start process")
-if button2:
-    st.success(f'Welcome to the reddit scraper for , {name} topic!')
-    subreddit = reddit.subreddit(name)
-    posts_dict = {"Title": [], "Post Text": [],
+button = st.button("click to start process")
+if button:
+	st.success(f'Welcome to the reddit scraper for , {name} topic!')
+        subreddit = reddit.subreddit(name)
+        posts_dict = {"Title": [], "Post Text": [],
           "ID": [], "Score": [],
-          "Total Comments": [], "Post URL": []
-          }
-    for submission in subreddit.hot(limit=3):
-      # Title of each post
-      posts_dict["Title"].append(submission.title)
+          "Total Comments": [], "Post URL": []}
+	for submission in subreddit.hot(limit=3):
+		posts_dict["Title"].append(submission.title)
 
       # Text inside a post
-      posts_dict["Post Text"].append(submission.selftext)
+      		posts_dict["Post Text"].append(submission.selftext)
 
       # Unique ID of each post
-      posts_dict["ID"].append(submission.id)
+      		posts_dict["ID"].append(submission.id)
 
       # The score of a post
-      posts_dict["Score"].append(submission.score)
+      		posts_dict["Score"].append(submission.score)
 
       # Total number of comments inside the post
-      posts_dict["Total Comments"].append(submission.num_comments)
+      		posts_dict["Total Comments"].append(submission.num_comments)
 
       # URL of each post
-      posts_dict["Post URL"].append(submission.url)
+      		posts_dict["Post URL"].append(submission.url)
+		
+      # Title of each post
       
-    df = pd.DataFrame(posts_dict) 
 	
-    st.title('Reddit Posts Search Filter')
+    
+      
+    	df = pd.DataFrame(posts_dict) 
+	
+    	st.title('Reddit Posts Search Filter')
 
     # Search input
-    search_word = st.text_input('Enter a search word:')
-    button = st.button("click to filter")
+    	search_word = st.text_input('Enter a search word:')
+	
+    	button2 = st.button("click to filter")
     # Filter the dataframe if a search word is entered
-    if button:
-	    mask = df.applymap(lambda x: search_word.lower() in str(x).lower()).any(axis=1)
-	    filtered_df = df[mask]
-	    st.write(mask)
-	    
-	    
-        # Use .applymap() to apply the search function to each cell of the DataFrame
-        # and .any(axis=1) to check if any cell in a row contains the search word
-         
-	  #
-          
-    else:
-	    filtered_df = df
-	    st.dataframe(filtered_df)
-	    
-        # If no search word is entered, display the original dataframe
-        
-	    
+    	if button2:
+		mask = df.applymap(lambda x: search_word.lower() in str(x).lower()).any(axis=1)
+		filtered_df = df[mask]
+		st.write(mask)
 
-    # Display the filtered dataframe
+    	else:
+		filtered_df = df
+		st.dataframe(filtered_df)
+		
+
     

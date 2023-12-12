9e675f16ef83f3dd1594e2543c1dc62e3ad48873
saeedbaba1368 <108682089+saeedbaba1368@users.noreply.github.com>
2023-12-13 00:10:48 +0330

    Update get_subreddit_data.py

--- get_subreddit_data.py
+++ get_subreddit_data.py
@@ -10,18 +10,16 @@ def get_subreddit_data(attr, reddit, topic, limit,time_filter):
         "Total Comments": [],
         "Post URL": []
     }
-
-    submissions = getattr(subreddit, attr)(limit=int(limit),time_filter=time_filter)
-
+    if attr == "top" or attr == "controversial" :
+        submissions = getattr(subreddit, attr)(limit=int(limit),time_filter=time_filter)
+    else:
+        submissions = getattr(subreddit, attr)(limit=int(limit))
     for submission in submissions:
-        subreddit_dict["Title"].append(submission.title)
-        subreddit_dict["Post Text"].append(submission.selftext)
-        subreddit_dict["ID"].append(submission.id)
-        subreddit_dict["Score"].append(submission.score)
-        subreddit_dict["Total Comments"].append(submission.num_comments)
-        subreddit_dict["Post URL"].append(submission.url)
+            subreddit_dict["Title"].append(submission.title)
+            subreddit_dict["Post Text"].append(submission.selftext)
+            subreddit_dict["ID"].append(submission.id)
+            subreddit_dict["Score"].append(submission.score)
+            subreddit_dict["Total Comments"].append(submission.num_comments)
+            subreddit_dict["Post URL"].append(submission.url)
 
     return pd.DataFrame(subreddit_dict)
-
-# Usage example:
-# df = get_subreddit_data('hot', reddit_instance, 'python', 10)

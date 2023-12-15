8d49599fadcf510ca844b3c5c32223faf7186318
saeedbaba1368 <108682089+saeedbaba1368@users.noreply.github.com>
2023-12-15 20:22:16 +0330

    1_👋_Reddit_Scraper_based on submission_id.py

--- pages/2_👋_test.py
+++ pages/2_👋_test.py
@@ -1,17 +1,62 @@
-
 import streamlit as st
 from df_global_search import DataFrameSearch
 from get_reddit import get_reddit
-from get_subreddit_date import get_subreddit_date
-
+from get_submission_data_by_id import get_submission_data_by_id
 reddit=get_reddit()
-df={}
+df_attr={}
+col1,col2 = st.columns(2)
+with col1:
+    submission_id = st.text_input('Submission_Id')
+with col2:
+    attr=st.selectbox('Choose :', ('comments', 'submission'))
+    
+button = st.button("Click to Start process")
 
 
-import pprint
+if button:
+	st.success(f'Welcome to the reddit scraper for , {submission_id} submission_id!')
 
-# assume you have a praw.Reddit instance bound to variable `reddit`
-submission = reddit.submission("18fhrre")
-st.write(submission.title)  # to make it non-lazy
-a=pprint.pprint(vars(submission))
-st.write(a)
+if submission_id :
+    df_submission, df_comments = get_submission_data_by_id(submission_id,reddit)
+    if attr=="comments":
+        df_attr=df_comments
+    else:
+        df_attr=df_submission
+    search_bar_columns_submission = st.columns((0.01, 1))
+    with search_bar_columns_submission[1]:
+        search_text_submission = st.text_input(
+				"Search", label_visibility="collapsed", placeholder="Search Text"
+			)
+
+    with DataFrameSearch(
+			dataframe=df_attr,
+			text_search=search_text_submission,
+			
+		) as df_attr:
+            st.dataframe(data=df_attr, use_container_width=True, hide_index=True)
+else:
+    st.write("enjoy Reddit scraper")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

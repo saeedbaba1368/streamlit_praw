1f766f5d27d2ee5d18ebe01e54a2e28af84b5f1d
saeedbaba1368 <108682089+saeedbaba1368@users.noreply.github.com>
2023-12-15 21:18:28 +0330

    Update 1_👋_submission_scraper_based_id.py

--- pages/1_👋_submission_scraper_based_id.py
+++ pages/1_👋_submission_scraper_based_id.py
@@ -1,42 +1,56 @@
-import streamlit as st
-from df_global_search import DataFrameSearch
-from get_reddit import get_reddit
-from get_submission_data_by_id import get_submission_data_by_id
-reddit=get_reddit()
-df_attr={}
-col1,col2 = st.columns(2)
-with col1:
-    submission_id = st.text_input('Submission_Id')
-with col2:
-    attr=st.selectbox('Choose :', ('comments', 'submission'))
+import pandas as pd
+def get_submission_data_by_id(submission_id,reddit):
+    # Initialize PRAW with a script-type authentication
+    # Fetch the submission
+    submission = reddit.submission(id=submission_id)
+
+    # Scrape all available information about the submission
+    submission_info = {
+        'title': [submission.title],
+        'score': [submission.score],
+        'id': [submission.id],
+        'url': [submission.url],
+        'author': [str(submission.author)],
+        'author_fullname': [submission.author_fullname],
+        'num_comments': [submission.num_comments],
+        'selftext': [submission.selftext],
+        'created_utc': [submission.created_utc],
+        'upvote_ratio': [submission.upvote_ratio],
+        'subreddit': [str(submission.subreddit)],
+        'link_flair_text': [submission.link_flair_text],
+        'is_original_content': [submission.is_original_content],
+        'over_18': [submission.over_18],
+    }
+
+    # Create a DataFrame for the submission
+    df_submission = pd.DataFrame(submission_info)
     
-button = st.button("Click to Start process")
+    # Scrape all comments from the submission
+    submission.comments.replace_more(limit=None)  # This line may take some time
+    comments_data = []
+    for comment in submission.comments.list():
+        
+        
+            comments_data.append({
+                'author': str(comment.author),
+                'body': comment.body,
+                'created_utc': comment.created_utc,
+                'score': comment.score,
+                'parent_id': comment.parent_id,
+                'is_submitter': comment.is_submitter,
+                'submission_id': submission_id,
+                
+            })
+            for reply in comment.replies:
+                comments_data.append({
+                    "comments_reply": reply.body})
+                    
+                
 
+    # Create a DataFrame for the comments
+    df_comments = pd.DataFrame(comments_data)
 
-if button:
-	st.success(f'Welcome to the reddit scraper for , {submission_id} submission_id!')
-
-if submission_id :
-    df_submission, df_comments = get_submission_data_by_id(submission_id,reddit)
-    if attr=="comments":
-        df_attr=df_comments
-    else:
-        df_attr=df_submission
-    search_bar_columns_submission = st.columns((0.01, 1))
-    with search_bar_columns_submission[1]:
-        search_text_submission = st.text_input(
-				"Search", label_visibility="collapsed", placeholder="Search Text"
-			)
-
-    with DataFrameSearch(
-			dataframe=df_attr,
-			text_search=search_text_submission,
-			
-		) as df_attr:
-            st.dataframe(data=df_attr, use_container_width=True, hide_index=True)
-else:
-    st.write("enjoy Reddit scraper")
-
+    return df_submission, df_comments
 
 
 
